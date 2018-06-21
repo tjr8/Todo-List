4c512b1455b3211a976d65ed8259cd9580d8d124
tjr8 <tjrdesigner@gmail.com>
2018-06-20 22:24:29 -0700

    Add initial js

---
+++ js.js
@@ -0,0 +1,8 @@
+// User clicked on the add button
+// Add text if filled
+document.querySelector('#add').addEventListener('click', () =>
+  var value = document.querySelector('#item').value;
+  if (value) {
+
+  }
+)
