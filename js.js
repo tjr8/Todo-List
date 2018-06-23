38065da08bdf1c4b5f25f0cb86ea09173638461a
tjr8 <tjrdesigner@gmail.com>
2018-06-23 15:45:50 -0700

    Stable css changes

--- js.js
+++ js.js
@@ -1,8 +1,10 @@
 // User clicked on the add button
 // Add text if filled
-document.querySelector('#add').addEventListener('click', () =>
+document.querySelector('#add').addEventListener('click', function() {
   var value = document.querySelector('#item').value;
-  if (value) {
+  if (value) addItemTodo(value);
+});
 
-  }
-)
+function addItemTodo(text) {
+  console.log(text);
+}
